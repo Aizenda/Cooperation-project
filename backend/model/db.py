ac9cf1c11693bb5b82d683890f8fa0a481b77b0c
Aizenda <qaz098380740669@gmail.com>
2025-04-29 22:24:34 +0800

    add go home js & radar

---
+++ backend/model/db.py
@@ -0,0 +1,35 @@
+import db_connector
+
+
+
+def create_database():
+    con = None
+    cursor = None
+    
+    try:
+        con = db_connector.mysql_pool.get_connection()  
+        cursor = con.cursor()
+        
+        create_table_query = """
+            CREATE TABLE IF NOT EXISTS radar_data (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                radar_img_url VARCHAR(255) NOT NULL,
+                radar_time DATETIME NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        """
+        cursor.execute(create_table_query)
+        con.commit()
+
+    except Exception as e:
+        print(f"Create table failed: {e}")
+
+    finally:
+        if cursor:
+            cursor.close()
+        if con:
+            con.close()
+            
+if __name__ == "__main__":
+	create_database()
+    
